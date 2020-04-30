e7edc8856663f40a13e08fdacfd31faf5b7f80ad
Malica Iralieva <fly-mukha@yandex.ru>
2020-04-30 13:09:42 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Stock from './stockElement.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Stock />
       </div>
     );
   }
---
+++ src/stockElement.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Stock extends Component {
+    render() {
+        return(
+            <div> Hello </div>
+        )
+    }
+}
